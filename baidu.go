823fe767c77a7d2328a9a69e4c200b7f195fe402
Chuanjian Wang <me@ckeyer.com>
2020-07-17 12:03:20 +0800

    update baidu sdk

--- baidu.go
+++ baidu.go
@@ -4,6 +4,7 @@ import (
 	"github.com/funxdata/baidu/core"
 	"github.com/funxdata/baidu/face"
 	"github.com/funxdata/baidu/nlp"
+	"github.com/funxdata/baidu/ocr"
 	"github.com/funxdata/baidu/speech"
 )
 
@@ -25,7 +26,12 @@ func (b *Baidu) Speech() *speech.BaiduSpeech {
 	return &speech.BaiduSpeech{b.Core}
 }
 
-// Face
+// NLP
 func (b *Baidu) NLP() *nlp.BaiduNLP {
 	return &nlp.BaiduNLP{b.Core}
+}
+
+// OCR .
+func (b *Baidu) OCR() *ocr.BaiduOCR {
+	return &ocr.BaiduOCR{b.Core}
 }
